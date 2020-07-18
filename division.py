ad3739027b475578b49128e1bf5f7175b8456784
Shantanu Khanzode <stud.shantanu19@gmail.com>
2020-07-18 19:20:00 +0530

    Initial Commit

---
+++ division.py
@@ -0,0 +1,9 @@
+def division(num1,num2):
+    result=num1/num2
+    return result
+def main():
+    print(division(10,2))
+    print("this is the division file")
+
+if __name__=="__main__":
+    main()
